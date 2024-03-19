ce95173f29881b954c633e3256eeea08971aac17
parkblacksheep <p7421772@naver.com>
2024-03-19 14:52:22 +0900

    fix(EmployeeVO) : update EmployeeVO 2

---
+++ src/main/java/org/hs/controller/EmpController.java
@@ -0,0 +1,16 @@
+package org.hs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/employee/")
+@Log4j
+@AllArgsConstructor
+public class EmpController {
+
+}
---
+++ src/main/java/org/hs/service/EmpService.java
@@ -0,0 +1,5 @@
+package org.hs.service;
+
+public interface EmpService {
+
+}
